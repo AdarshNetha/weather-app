4c91a00ae83fd4ba3feb31ba741a0a5d3b8e0e57
AdarshNetha <abhiadarshnetha849@gmail.com>
2025-11-16 18:59:27 +0530

    added some code

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 let lat=null;
 let long=null;
+let apikey="6a8226f8301c98110488271aae60cde4"
 
 let getloc=()=>{
     navigator.geolocation.getCurrentPosition((position)=>{
         lat=position.coords.latitude
         long=position.coords.longitude;
-            console.log(lat);
-    console.log(long);
-    
+      
+    weather();
     })
-
-
-
 }
+getloc();
 let weather=async()=>{
-    let api=fetch(``)
+  let api=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apikey}`)
+  console.log(await api.json());
 }
